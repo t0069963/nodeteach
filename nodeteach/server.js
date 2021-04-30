e82899e93addb2e5b7b32b132fb5e0f8ba00ae98
bgete <erictinadogcat@yahoo.com.tw>
2021-04-30 17:07:54 +0800

    第二節點 #創建基本結構並安裝 ejs 套件和 express 套件 express 套件為 Web Framework  為中間層 主要作用為支援網頁開發上的函式庫 處理如cookie...等數據解析 ejs套件為產生 動態網頁 將程式碼分別離城範本跟資料

--- nodeteach/server.js
+++ nodeteach/server.js
@@ -1,6 +1,12 @@
 'use strict';
+///////////ず场家舱闽///////////////////////
+//更ず场家舱http
 var http = require('http');
+
+//秈祘吏挂
 var port = process.env.PORT || 1337;
+
+//////////////////////////////////////////////
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
