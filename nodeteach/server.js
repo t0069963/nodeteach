8a17af513096e11eb513e02b7f039568a2d0a15e
bgete <erictinadogcat@yahoo.com.tw>
2021-05-02 12:59:19 +0800

    第三節點 #載入express模組 設定視圖引擎為ejs 並新增主頁 關於我 及錯誤頁面

--- nodeteach/server.js
+++ nodeteach/server.js
@@ -5,10 +5,33 @@ var http = require('http');
 
 //宣告進程環境
 var port = process.env.PORT || 1337;
-
 //////////////////////////////////////////////
+//宣告express載入模組express
+var express = require('express');
+//宣告app使用express模組函數
+var app = express();
+//使用express.Router()創建一個新物件為router
+var router = express.Router();
+//設定視圖引擎為ejs *注意如果沒有設定views則預設路徑為/views
+app.set('view engine', 'ejs');
+//使用預設路徑並回叫router
+app.use('/', router);
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+//////
+
+router.get('/', function (req, res) {
+    res.render("index")
+});
+
+
+router.get('/about', function (req, res) {
+    res.render("about")
+});
+
+
+
+app.get('*', function (req, res) {
+    res.render("error")
+});
+app.listen(port);
+console.log(`server listening on:${port}`);
